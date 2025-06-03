148617e2ecd1eeeb3456adbab69ae7b3561d8a8e
Alex Necsoiu <alex-necsoiu@users.noreply.github.com>
2025-06-03 22:28:40 +0200

    Fix vercel deployment

--- lib/providers/ThemeProvider.tsx
+++ lib/providers/ThemeProvider.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+};
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
